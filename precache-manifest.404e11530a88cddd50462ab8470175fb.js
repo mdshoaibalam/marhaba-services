7bb58b0db2e80f37de4bd8cc441087224c0d1999
Shoaib <mdshoaib.alam22@gmail.com>
2020-10-17 18:31:39 +0530

    Updates

---
+++ precache-manifest.404e11530a88cddd50462ab8470175fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96471c0e4ca4713aa8de8eb6d1c34917",
+    "url": "/marhabaservices/index.html"
+  },
+  {
+    "revision": "8e5e54dd107f9808e1ee",
+    "url": "/marhabaservices/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "772bb65abd9876eab797",
+    "url": "/marhabaservices/static/js/2.7952f39c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/marhabaservices/static/js/2.7952f39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e5e54dd107f9808e1ee",
+    "url": "/marhabaservices/static/js/main.263d53b8.chunk.js"
+  },
+  {
+    "revision": "6a7d246503f5824e2ed1",
+    "url": "/marhabaservices/static/js/runtime-main.f8c04712.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marhabaservices/static/media/logo.5d5d9eef.svg"
+  }
+]);
