9166c5682b7b6fd2a79e3463306319c470e545d7
Shoaib <mdshoaib.alam22@gmail.com>
2020-10-17 20:15:56 +0530

    Updates

---
+++ precache-manifest.edbc5c4c19b1b27663ff9acbecc32259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8160577c980c84f3418b440dfb0f4a9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0292e2086238e7363e32",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "772bb65abd9876eab797",
+    "url": "/static/js/2.7952f39c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7952f39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0292e2086238e7363e32",
+    "url": "/static/js/main.7bdd3c32.chunk.js"
+  },
+  {
+    "revision": "f6ea10961593a5c7d062",
+    "url": "/static/js/runtime-main.d7abf94d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marhaba-services/precache-manifest.7d3e95f6d99f6f2914cf48ff83a92259.js"
+  "/precache-manifest.edbc5c4c19b1b27663ff9acbecc32259.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marhaba-services/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
